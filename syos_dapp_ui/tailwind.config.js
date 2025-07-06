313897e822e0058809cf5ac1d5334d91c47796a9
Himanshu <87159416+hash02@users.noreply.github.com>
2025-07-05 17:31:43 -0700

    Fix postcss config for tailwind

---
+++ syos_dapp_ui/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        anchor: '#5F4B8B',
+        drift: '#FF6B6B',
+        base: '#101010',
+      },
+    },
+  },
+  plugins: [],
+}
